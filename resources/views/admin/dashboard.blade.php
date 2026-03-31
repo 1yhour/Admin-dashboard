040509f618fe6014135eff984bfb90d01bce1b07
LYHOUR SENG <lyhourlucky77@gmail.com>
2026-03-31 14:50:20 +0700

    feat: add dashboard and user pages

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,5 @@
+<x-layout title="Dashboard">
+  <div>
+    this is dashboard page
+  </div>
+</x-layout>
---
+++ resources/views/admin/user.blade.php
@@ -0,0 +1,5 @@
+<x-layout title="User">
+  <div>
+    this is user page
+  </div>
+</x-layout>
